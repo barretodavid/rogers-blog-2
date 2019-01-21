7a7874d24ed5a35b48f33d6df4141b9ee19d407c
barretodavid <barretollano@gmail.com>
2019-01-21 16:33:47 -0500

    Added create post validation

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
@@ -11,7 +11,10 @@ export class CreatePostComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.postForm = this.fb.group({ title: [''], content: [''] });
+    this.postForm = this.fb.group({
+      title: ['', Validators.required],
+      content: ['', [Validators.required, Validators.minLength(5)]],
+    });
   }
 
   onSubmit() {
@@ -20,5 +23,13 @@ export class CreatePostComponent implements OnInit {
 
   onReset() {
     this.postForm.reset();
+  }
+
+  get title() {
+    return this.postForm.get('title');
+  }
+
+  get content() {
+    return this.postForm.get('content');
   }
 }
